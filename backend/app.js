4d2005c1b5be0ef9d79fc3bf704bfb140a595599
Edilásio Fortunato Carlos <ecarlos.najareal@gmail.com>
2019-03-18 13:01:28 +0100

    Config

--- backend/app.js
+++ backend/app.js
@@ -17,16 +17,27 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost/post-cat', {
+// mongoose.connect('mongodb://localhost/post-cat', {
+//     useNewUrlParser: true
+//   })
+//   .then(() => {
+//     console.log(`Server succefully connected to the database! `);
+//   })
+//   .catch(err => {
+//     console.log(`The conection as fail`);
+//     console.log(err);
+//   });
+
+mongoose.connect(
+  'mongodb://node-shop-user:' +
+  process.env.MONGO_ATLAS_PW +
+  '@node-rest-shop-shard-00-00-b37e1.mongodb.net:27017,node-rest-shop-shard-00-01-b37e1.mongodb.net:27017,node-rest-shop-shard-00-02-b37e1.mongodb.net:27017/post-cat-db?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true
-  })
-  .then(() => {
-    console.log(`Server succefully connected to the database! `);
-  })
-  .catch(err => {
-    console.log(`The conection as fail`);
-    console.log(err);
-  });
+  }
+).catch(error => {
+  console.log(`The DB conection as fail`);
+  console.log(error);
+});
 
 // app.use(bodyParser);
 app.use(bodyParser.json());
